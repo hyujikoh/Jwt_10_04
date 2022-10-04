55ef81069d1b7a9c5e0ff6775e5e71cb31c432a8
hyujikoh <dvum0045@gmail.com>
2022-10-04 15:29:52 +0900

    public SecretKey getSecretKey()
    메서드를 private 로 변경해주세요.
    
    t3, t4에 오류 발생
    - 이것도 해결해 보세요.
      - 자바 리플렉션을 이용해서 private 메서드를 외부에서 강제로 호출
    
    단, 테스트 환경에서만 사용해라

---
+++ src/main/java/com/ll/exam/app__2022_10_04/jwt/JwtProvider.java
@@ -0,0 +1,21 @@
+package com.ll.exam.app__2022_10_04.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    private final SecretKey jwtSecretKey;
+
+    private SecretKey getSecretKey() {
+        return jwtSecretKey;
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app__2022_10_04/JwtTest.java
@@ -0,0 +1,53 @@
+package com.ll.exam.app__2022_10_04;
+
+import com.ll.exam.app__2022_10_04.jwt.JwtProvider;
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class JwtTests {
+    @Value("${custom.jwt.secretKey}")
+    private String secretKeyPlain;
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Test
+    @DisplayName("secretKey 키가 존재해야한다.")
+    void t1() {
+        assertThat(secretKeyPlain).isNotNull();
+    }
+
+    @Test
+    @DisplayName("sercretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
+    void t2() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+        System.out.println(secretKey);
+        assertThat(secretKey).isNotNull();
+    }
+    @Test
+    @DisplayName("JwtProvider 객체로 SecretKey 객체를 생성할 수 있다.")
+    void t3() {
+
+        SecretKey secretKey = TestUtil.callMethod(jwtProvider, "getSecretKey");
+
+        assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("SecretKey 객체는 단 한번만 생성되어야 한다.")
+    void t4() {
+        SecretKey secretKey1 = TestUtil.callMethod(jwtProvider, "getSecretKey");
+        SecretKey secretKey2 = TestUtil.callMethod(jwtProvider, "getSecretKey");
+
+        assertThat(secretKey1 == secretKey2).isTrue();
+    }
+}
